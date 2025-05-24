1105a11bf51181cd9be350811c64be071fd31f9f
quangnha330011002005 <trranquangnha@gmail.com>
2025-05-24 19:28:44 +0700

    Load_More

--- EFCORE/Controllers/HomeController.cs
+++ EFCORE/Controllers/HomeController.cs
@@ -1,32 +1,33 @@
-using System.Diagnostics;
 using EFCORE.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace EFCORE.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
 
-        public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _db = db;
         }
-
         public IActionResult Index()
         {
-            return View();
+            var pageSize = 4;
+            var dsSanPham = _db.Products.Include(x => x.Category).ToList();
+            return View(dsSanPham.Skip(0).Take(pageSize).ToList());
         }
-
+        public IActionResult LoadMore(int page = 1)
+        {
+            var pageSize = 4;
+            var dsSanPham = _db.Products.Include(x => x.Category).ToList();
+            return PartialView("_ProductPartial", dsSanPham.Skip((page - 1) * pageSize).Take(pageSize).ToList());
+        }
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
